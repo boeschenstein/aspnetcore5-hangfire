a7bea77ecda64b0a59a974a6cdce6eab0966b31e
Boeschenstein, Patrik <patrik.boeschenstein@hotmail.com>
2020-12-06 16:38:39 +0100

    get connection string from appsettings

---
+++ MyHFConsole/App.cs
@@ -0,0 +1,33 @@
+﻿using Hangfire;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace MyHFConsole
+{
+    public class App
+    {
+        private readonly IConfigurationRoot configuration;
+
+        public App(IConfigurationRoot configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public Task Run()
+        {
+            Console.WriteLine("Hello World!");
+
+            var dummy = new MyHFTest.CustomHelloWorld(); // to start the recurring test job configured in MyHFTest, a reference to this class needed!
+
+            GlobalConfiguration.Configuration.UseSqlServerStorage(configuration.GetConnectionString("HangfireConnection"));
+
+            using (var server = new BackgroundJobServer())
+            {
+                Console.WriteLine("Hangfire Server started. Press any key to exit...");
+                Console.ReadKey();
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
--- MyHFConsole/Program.cs
+++ MyHFConsole/Program.cs
@@ -1,23 +1,58 @@
-﻿using Hangfire;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace MyHFConsole
 {
-    internal class Program
+    internal static class Program
     {
-        private static void Main(string[] args)
+        private static IConfigurationRoot configuration;
+
+        private static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            var dummy = new MyHFTest.CustomHelloWorld(); // to start the recurring test job, a reference to this class needed!
-
-            GlobalConfiguration.Configuration.UseSqlServerStorage("Server=(localdb)\\mssqllocaldb;Database=HangfireTest;Integrated Security=SSPI;");
-
-            using (var server = new BackgroundJobServer())
+            try
             {
-                Console.WriteLine("Hangfire Server started. Press any key to exit...");
-                Console.ReadKey();
+                MainAsync(args).Wait();
+                return 0;
             }
+            catch
+            {
+                return 1;
+            }
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            // Create service collection
+            ServiceCollection serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            // Create service provider
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+            // Print connection string to demonstrate configuration object is populated
+            Console.WriteLine(configuration.GetConnectionString("HangfireConnection"));
+
+            await serviceProvider.GetService<App>().Run();
+        }
+
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            // Build configuration
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                //.AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true)
+                //.AddEnvironmentVariables()
+                .Build();
+
+            // Add access to generic IConfigurationRoot
+            serviceCollection.AddSingleton<IConfigurationRoot>(configuration);
+
+            // Add app
+            serviceCollection.AddTransient<App>();
         }
     }
 }
