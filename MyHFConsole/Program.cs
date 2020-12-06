2ac4ebf8147cd3a7fdc63c1f68eef7623606b3f4
Boeschenstein, Patrik <patrik.boeschenstein@hotmail.com>
2020-12-06 16:06:18 +0100

    run hangfire server in console

---
+++ MyHFConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using Hangfire;
+using System;
+
+namespace MyHFConsole
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var dummy = new MyHFTest.CustomHelloWorld(); // to start the recurring test job, a reference to this class needed!
+
+            GlobalConfiguration.Configuration.UseSqlServerStorage("Server=(localdb)\\mssqllocaldb;Database=HangfireTest;Integrated Security=SSPI;");
+
+            using (var server = new BackgroundJobServer())
+            {
+                Console.WriteLine("Hangfire Server started. Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
